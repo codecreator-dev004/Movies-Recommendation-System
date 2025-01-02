2c054672b24dfdc02b9be6b7911fd3ab0ce24479
Devanshu Kumar <kumardevanshu553@gmail.com>
2025-01-02 20:21:02 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 with open("README.md","r", encoding = "utf-8")as fh:
     long_description = fh.read()
 
-REPO_NAME = "Movies-Recommendationr-System
-AUTHOR_NAME = 'DEVANSHU KUMAR',
-SRC_REPO = 'src',
+REPO_NAME = "Movies-Recommendationr-System"
+AUTHOR_NAME = "DEVANSHU KUMAR"
+SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = ['streamlit']
 
 setup(
@@ -12,11 +12,12 @@ setup(
     name = SRC_REPO,
     version = '0.0.1',
     author = AUTHOR_NAME,
-    author_email = 'kumardevanshu553@gmail.com',
     description = 'A Simple package for Movie Recommendation System',
     long_description = long_description,
     long_description_content_type = 'text/markdown',
-    package = [SRC_REPO],
-    python_requires = '>=3.12',
+    url = f"https://github.com/{AUTHOR_NAME}/{REPO_NAME}"
+    author_email = 'kumardevanshu553@gmail.com',
+    packages = [SRC_REPO],
+    python_requires = ">=3.12",
     install_requires = LIST_OF_REQUIREMENTS
 )
